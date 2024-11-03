9d4a69b39695d56621de02c432575327722ebaf4
Merylad <meryl.adjedj@gmail.com>
2024-11-03 14:07:04 +0200

    W1 D1  XP

---
+++ Week1/Day1/XP/file.py
@@ -0,0 +1,84 @@
+#########################
+#EX1
+#########################
+
+# print('Hello World\n'*4)
+
+#########################
+#EX2
+#########################
+
+# print((99**3)*8)
+
+#########################
+#EX3
+#########################
+
+# >>> 5 < 3   TRUE
+# >>> 3 == 3   TRUE
+# >>> 3 == "3"  FALSE
+# >>> "3" > 3  ERROR
+# >>> "Hello" == "hello" FALSE
+
+#########################
+#EX4
+#########################
+
+# computeur_brand = 'HP'
+# print(f'I have a {computeur_brand} computeur.')
+
+#########################
+#EX5
+#########################
+
+# name='Meryl'
+# age=35
+# shoe_size=40
+
+# info=f'Hi my name is {name}, i am {age} years old and my shoe size is {shoe_size}'
+
+# print(info)
+
+#########################
+#EX6
+#########################
+
+# a=6
+# b=5
+
+# if a>b:
+#     print('Hello world')
+
+#########################
+#EX7
+#########################
+
+# num=int(input('Give me a number :'))
+
+# if num%2 == 0 :
+#     print('Your number is even')
+# else:
+#     print('Your number is odd')
+
+#########################
+#EX8
+#########################
+
+# name = input('Write your name :')
+
+# if name.lower() == 'meryl':
+#     print('WWWOOOOOOOWWWW We have the same name ! Meryl is such a beautiful name!')
+# else:
+#     print(f'Hi {name.capitalize()}')
+
+#########################
+#EX9
+#########################
+
+# height = int(input('Write your height in cm :'))
+
+# if height >= 145 :
+#     print('You are tall enough to ride')
+# else:
+#     print('You need to grow!')
+
