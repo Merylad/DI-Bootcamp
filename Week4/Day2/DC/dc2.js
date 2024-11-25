c82eff371280a9859ddeb7caf26dca9c4f6fa181
Merylad <meryl.adjedj@gmail.com>
2024-11-25 19:11:22 +0200

    DC2

---
+++ Week4/Day2/DC/dc2.js
@@ -0,0 +1,14 @@
+let a = ''
+for (let i = 0; i<6; i++ ){
+    a+='* '
+    console.log(a)
+}
+
+
+for (let i = 0; i<6; i++ ){
+    let b = ''
+    for (let j = 0; j<=i; j++ ){
+        b+='* '
+    }
+    console.log(b)
+}
