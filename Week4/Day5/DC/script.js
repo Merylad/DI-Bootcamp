126c34d29d5ef6e42d1541f75c745dcc326aadef
Merylad <meryl.adjedj@gmail.com>
2024-11-28 21:41:39 +0200

    DC

---
+++ Week4/Day5/DC/script.js
@@ -0,0 +1,61 @@
+let myForm = document.getElementById('myForm')
+let userTask = document.getElementById('userTask')
+let myTasks = document.getElementById('myTasks')
+
+
+
+myForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    const taskText = userTask.value;
+
+    let div = document.createElement('div');
+    div.setAttribute('class', 'row container thisDiv')
+    
+    let deleteBtn = document.createElement('button');
+    let checkbox = document.createElement('input');
+    let span = document.createElement('span')
+    let label = document.createElement('label')
+    span.textContent = taskText
+    
+    checkbox.setAttribute('type', 'checkbox');
+    
+
+
+    
+    deleteBtn.setAttribute('class', 'btn waves-effect red deleteBtn');
+    let i = document.createElement('i');
+    i.textContent = 'delete';
+    i.setAttribute('class', 'material-icons');
+    deleteBtn.appendChild(i);
+
+    
+
+    // Append everything to the task container
+    label.appendChild(checkbox)
+    label.appendChild(span)
+    div.appendChild(label)
+    div.appendChild(deleteBtn); 
+    myTasks.appendChild(div);
+
+    
+    userTask.value = '';
+
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            span.style.textDecoration = 'line-through';
+        } else {
+            span.style.textDecoration = 'none';
+        }
+    });
+
+    deleteBtn.addEventListener('click', function(){
+        myTasks.removeChild(div)
+    })
+
+    
+})
+
+
+
+
